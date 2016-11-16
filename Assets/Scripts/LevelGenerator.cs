e8735bb807637799e0a45ff5a37fce26a0a0be2e
Bradley Trowbridge <bradley.trowbridge@yahoo.com>
2016-11-16 14:47:16 -0500

    Initial commit

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class LevelGenerator : MonoBehaviour {
+
+    public int Height = 128;
+    public int Width = 128;
+
+    [Range(1,8)]
+    public int OctaveCount = 5;
+
+	void Start () {
+        GenerateLevel();
+	}
+
+    private void GenerateLevel()
+    {
+        mTexture = new Texture2D(Height, Width, TextureFormat.RGB24, false);
+
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = mTexture;
+
+        float[][] perlinNoise = PerlinNoise.GeneratePerlinNoise(Height, Width, OctaveCount);
+        for (int i = 0; i<Height; i++)
+        {
+            for (int j = 0; j<Width; j++)
+            {
+                float noise = perlinNoise[i][j];
+                mTexture.SetPixel(i, j, new Color(noise, noise, noise));
+            }
+        }
+        mTexture.Apply();
+    }
+
+    private Texture2D mTexture;
+}
---
+++ Assets/Scripts/MinecraftLevelGenerator.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinecraftLevelGenerator : MonoBehaviour {
+
+    public int WaterLevel = 0;
+    public int SnowLevel = 5;
+    public int DirtThickness = 5;
+
+    [Range(1,20)]
+    public int HeightScale = 10;
+
+    public int MinDepth = -10;
+    public int LevelHeight = 50;
+    public int LevelWidth = 50;
+
+    [Range(1, 8)]
+    public int OctaveCount = 5;
+
+    public GameObject WaterCube;
+    public GameObject DirtCube;
+    public GameObject SnowCube;
+    public GameObject GrassCube;
+    public GameObject StoneCube;
+
+    void Start () {
+        GenerateLevel();
+	}
+	
+    private void GenerateLevel()
+    {
+        float[][] perlinNoise = PerlinNoise.GeneratePerlinNoise(LevelHeight, LevelWidth, OctaveCount);
+        for (int z = 0; z < LevelHeight; z++)
+        {
+            for (int x = 0; x < LevelWidth; x++)
+            {
+                float y = (perlinNoise[z][x] * HeightScale);
+                Vector3 blockPosition = new Vector3(x, Mathf.RoundToInt(y), z);
+                CreateBlock(blockPosition);
+
+            }
+        }
+    }
+    private void CreateBlock(Vector3 surfacePosition)
+    {
+        GameObject surfaceBlock = null;
+
+        if (surfacePosition.y >= SnowLevel)
+        {
+            GameObject.Instantiate(SnowCube, surfacePosition, Quaternion.identity);
+        } else if (surfacePosition.y < WaterLevel)
+        {
+            surfaceBlock = DirtCube;
+            //Fill Water
+            for(int y = (int)surfacePosition.y + 1; y <= WaterLevel; y++)
+            {
+                Vector3 waterPosition = new Vector3(surfacePosition.x, y, surfacePosition.z);
+                GameObject.Instantiate(WaterCube, waterPosition, Quaternion.identity);
+            }
+        } else {
+            surfaceBlock = GrassCube;
+        }
+        GameObject.Instantiate(surfaceBlock, surfacePosition, Quaternion.identity);
+       
+        //Fill ground
+        for(int depthY = MinDepth; depthY < surfacePosition.y; depthY++)
+        {
+            GameObject undergroundBlock = null;
+            if (surfacePosition.y - depthY  <= DirtThickness)
+            {
+                undergroundBlock = DirtCube;
+            }
+            else
+            {
+                undergroundBlock = StoneCube;
+            }
+            Vector3 undergroundPosition = new Vector3(surfacePosition.x, depthY, surfacePosition.z);
+            GameObject.Instantiate(undergroundBlock, undergroundPosition, Quaternion.identity);
+        }
+    }
+
+
+
+}
